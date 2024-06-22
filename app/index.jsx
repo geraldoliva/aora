b8057818c94baa80e103a94cb15e5d1a9c181769
Gerald Oliva <geraldsamoliva@gmail.com>
2024-06-22 18:01:32 -0400

    Add bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,15 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Got to Profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Got to Home
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
